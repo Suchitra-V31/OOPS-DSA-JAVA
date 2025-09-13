e01089acf1b1ed6cc2b9cea2b80aff3d7ded2ce1
Suchitra V <88223440+Suchitra-V31@users.noreply.github.com>
2025-09-13 19:59:11 +0530

    Add files via upload

---
+++ OOPS/DigitalCamera/Tester.java
@@ -0,0 +1,8 @@
+package digitalCamera;
+
+public class Tester {
+	public static void main(String[] args) {
+        DigitalCamera camera = new DigitalCamera("Canon",100);
+        System.out.println(camera.getBrand()+" "+camera.getCost()+" "+camera.getMemory());
+    }
+}
